2ecca0774da7436294ddec3d8d5c28ab2291c4d8
Rick Walters <rick@yonderwindow.com>
2018-12-28 14:41:15 -0500

    multi-value slider: value interdependence pathways have now been established between sliders (but the calculation to distribute change hasn't been implemented properly yet.

--- Multi-Value Sliders/slider-control.js
+++ Multi-Value Sliders/slider-control.js
@@ -1,15 +1,35 @@
 autowatch = 1
 outlets = 2;
 var totalSliderCount = 10;
+var activeSliders = 10;
+var persistedValues = {
+  minimumValue: 0, // default, should be reset by patcher on load
+  maximumValue: 100, // default, should be reset by patcher on load
+  sliderValues: []};
+  for(var i=0; i < 10; i++) {
+    persistedValues.sliderValues[i] = 0;
+  }
+var valuePrecision = 4;
 
 setoutletassist (0, 'all value messages\nsend these directly to the live.slider objects');
 setoutletassist (1, 'all attribute-related messages\nsend these to the attrUI objects connected to each live.slider');
 
+function dump() {
+  post('dump', 'persistedValues: \n');
+  for(var p in persistedValues) {
+    post('\t' + p + ' = ' + (typeof persistedValues[p] === 'Array' ? persistedValues[p].join(',') : persistedValues[p]) + '\n');
+  }
+}
+
 function setSliderCount(activeSliderCount) {
+  activeSliders = activeSliderCount;
   setPropertiesForAllSliders(totalSliderCount, activeSliderCount);
 }
 
 function setSliderRanges(newMax, newMin) {
+  persistedValues.minimumValue = newMin;
+  persistedValues.maximumValue = newMax;
+  post('state', 'setting new range: ' + persistedValues.minimumValue + ', ' + persistedValues.maximumValue, '\n');
   for(var i=0; i < totalSliderCount; i++) {
     outlet(0, [i, '_parameter_range', newMax, newMin]);
   }
@@ -18,6 +38,8 @@ function setSliderRanges(newMax, newMin) {
 function setPropertiesForAllSliders(numberOfSliders, activeSliderCount) {
   for(var i = 0; i < numberOfSliders; i++) {
     setSliderColors(i, activeSliderCount > i);
+    outlet(1, [i, 'attr', 'ignoreclick']);
+    outlet(1, [i, activeSliderCount > i ? 0 : 1]);
   }
 }
 
@@ -39,4 +61,36 @@ function setSliderColors(sliderIndex, enabled) {
     outlet(1, [sliderIndex, 'attr', propertyValues[i][0]]);
     outlet(1, [sliderIndex, propertyValues[i][1][0], propertyValues[i][1][1], propertyValues[i][1][2], opacity])
   };
+}
+
+function distributeSliderValueChange(changedIndex, newValue) {
+  persistedValues.sliderValues[changedIndex] = newValue;
+}
+
+function sliderValueChanged(sliderIndex, newValue) {
+  post('slider ' + sliderIndex + ' changed to ' + newValue + '\n');
+  const roundedValue = roundSliderValue(newValue);
+  if(roundedValue !== persistedValues.sliderValues[sliderIndex]) {
+  //   post('will record slider ' + sliderIndex + ' value as ' + roundedValue + '\n');
+    var oldValue = persistedValues.sliderValues[sliderIndex];
+    distributeSliderValueChange(sliderIndex, roundedValue);
+    resendSliderValues();
+  }
+}
+
+function setSliderValue(sliderIndex, newValue) {
+  persistedValues.sliderValues[sliderIndex] = newValue;
+  outlet(0, [sliderIndex, 'set', persistedValues.minimumValue + newValue * persistedValues.maximumValue]);
+}
+
+function resendSliderValues() {
+  for(var i = 0; i < persistedValues.sliderValues.length; i++) {
+    post(i + '\n');
+    outlet(0, [i, 'set', persistedValues.minimumValue + persistedValues.sliderValues[i] * persistedValues.maximumValue]);
+  }
+}
+
+function roundSliderValue(value) {
+  const roundingFactor = Math.pow(10, valuePrecision);
+  return Math.round(value * roundingFactor) / roundingFactor;
 }
